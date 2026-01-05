acb7ad7cfd8b5bc84a286663f31167903aa7c9a8
tabbyblack <tb789@cam.ac.uk>
2026-01-05 19:43:46 +0000

    cyclegan repo merged into this one

--- train.py
+++ train.py
@@ -5,6 +5,35 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
 
+"""General-purpose training script for image-to-image translation.
+
+This script works for various models (with option '--model': e.g. cyclegan, colorization) and
+different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
+You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
+
+It first creates model, dataset, and visualizer given the option.
+It then does standard network training. During the training, it also visualize/save the images, print/save the loss plot, and save models.
+The script supports continue/resume training. Use '--continue_train' to resume your previous training.
+
+Example:
+    Train a CycleGAN model:
+        python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
+    Train a pix2pix model:
+        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
+
+See options/base_options.py and options/train_options.py for more training options.
+See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
+See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
+"""
+
+import time
+from options.train_options import TrainOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import Visualizer
+from util.util import init_ddp, cleanup_ddp
+
+# I have not adapted the rest of this to match the same train.py script as in CycleGAN repo but it should work. cycle_gan_model.py (and rest of files) are the same in both repos so this training script should work 
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
