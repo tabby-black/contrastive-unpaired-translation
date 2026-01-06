c724cee7898dbb7fef74eab74c77eb5d5abc35d2
tabbyblack <tb789@cam.ac.uk>
2026-01-06 12:15:43 +0000

    changing paths of data directories

--- datasets/organise_data.sh
+++ datasets/organise_data.sh
@@ -1,24 +1,25 @@
 # this script helps the user download and set up the dataset as required for calibration etc.
 
 # DATASET_DIR is the relative path to the HistologyHSI-GB dataset
+# this file is being run from inside datasets
 DATASET_DIR="../../datasets/'PKG - HistologyHSI-GB'"
-RAW_DIR="./datasets/raw"
-WHITE_DIR="./datasets/white"
-DARK_DIR="./datasets/dark"
-PREPROCESSED_DIR="./datasets/preprocessed"
-RGB_DIR="./datasets/rgb"
+RAW_DIR="./raw"
+WHITE_DIR="./white"
+DARK_DIR="./dark"
+PREPROCESSED_DIR="./preprocessed"
+RGB_DIR="./rgb"
 # train and test datasets should be inside ./datasets/histology for training purposes
-TRAIN_A="./datasets/histology/trainA"
-TEST_A="./datasets/histology/testA"
-TRAIN_B="./datasets/histology/trainB"
-TEST_B="./datasets/histology/testB"
+TRAIN_A="./histology/trainA"
+TEST_A="./histology/testA"
+TRAIN_B="./histology/trainB"
+TEST_B="./histology/testB"
 # create expected dataset folder structure for preprocessing
 mkdir -p ${RAW_DIR} ${WHITE_DIR} ${DARK_DIR} ${PREPROCESSED_DIR} ${RGB_DIR} ${TRAIN_A} ${TEST_A} ${TRAIN_B} ${TEST_B}
 
 
 
 
-# the dataset should have already been downloaded into /datasets/temp
+# HistologyHSI-GB dataset is downloaded into ../../../datasets/HistologyHSI-GB
 
 # Step 1: Organise files into subfolders
 # rename files then move
@@ -36,7 +37,7 @@ for hdr_path in ../../../datasets/HistologyHSI-GB/P*/ROI*/raw.hdr; do
 done
 
 # organise hsi files
-for hdr_path in "${DATASET_DIR}"/temp/P*/ROI*/raw; do
+for hdr_path in ../../../datasets/HistologyHSI-GB/P*/ROI*/raw; do
     # extract P* and ROI* parts from path
     # ROI* part includes cover image number and tumor marker  eg. ROI_1_C01_T
     patient=$(basename "$(dirname "$(dirname "$hdr_path")")")
@@ -48,7 +49,7 @@ for hdr_path in "${DATASET_DIR}"/temp/P*/ROI*/raw; do
 done
 
 # organise whiteReference files
-for hdr_path in "${DATASET_DIR}"/temp/P*/ROI*/whiteReference.hdr; do
+for hdr_path in ../../../datasets/HistologyHSI-GB/P*/ROI*/whiteReference.hdr; do
     # extract P* and ROI* parts from path
     patient=$(basename "$(dirname "$(dirname "$hdr_path")")")
     roi=$(basename "$(dirname "$hdr_path")")
@@ -59,7 +60,7 @@ for hdr_path in "${DATASET_DIR}"/temp/P*/ROI*/whiteReference.hdr; do
 done
 
 # organise darkReference files
-for hdr_path in "${DATASET_DIR}"/temp/P*/ROI*/darkReference.hdr; do
+for hdr_path in ../../../datasets/HistologyHSI-GB/P*/ROI*/darkReference.hdr; do
     # extract P* and ROI* parts from path
     patient=$(basename "$(dirname "$(dirname "$hdr_path")")")
     roi=$(basename "$(dirname "$hdr_path")")
